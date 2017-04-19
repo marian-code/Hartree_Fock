9cd4ced4de453ec7030948b2837c30fc5c638103
Marián Rynik <marian.rynik@outlook.sk>
2017-04-19 08:13:26 +0200

    finalna verzia, vlnova funkcia nesplna lavu okrajovu podmienku - komentar v kode

--- Hartree_Fock/Hartree_Fock.cpp
+++ Hartree_Fock/Hartree_Fock.cpp
@@ -1,4 +1,4 @@
-// Hartree_Fock.cpp : Defines the entry point for the console application.
+ï»¿// Hartree_Fock.cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
@@ -10,7 +10,9 @@
 #include <Eigen\Core>
 #include <Eigen\Dense>
 #include <Eigen\Eigenvalues>
+#include <SymEigsSolver.h>  // Also includes <MatOp/DenseSymMatProd.h>
 
+using namespace Spectra;
 using namespace Eigen;
 using namespace std;
 
@@ -26,7 +28,7 @@ double Integral(VectorXd Wave_function, const double h_step, int i, const int ma
 	{
 		density = 0;
 		//spocitat hustotu pravdepodobnosti
-		for (k = 1; k < j; k++)
+		for (k = 0; k < j; k++)
 			density += h_step*pow(Wave_function(k), 2);
 		
 		//integral += (h_step*density)*(1.0/pow(double(j*h_step), 2)) / density_norm;
@@ -44,7 +46,7 @@ double Density_norm(VectorXd Wave_function, const double h_step, const int matri
 	
 	//spocitat normovanie hustoty pravdepodobnosti
 	density_norm = 0;
-	for (k = 1; k < matrix_size; k++)
+	for (k = 0; k < matrix_size; k++)
 		density_norm += h_step*pow(Wave_function(k), 2);
 	return density_norm;
 }
@@ -84,34 +86,40 @@ double Energy(VectorXd Wave_function, double energy, const int matrix_size, cons
 	{
 		density = 0;
 		//spocitat hustotu pravdepodobnosti
-		for (k = 1; k < j; k++)
+		for (k = 0; k < j; k++)
 			density += h_step*pow(Wave_function(k), 2);
 
 		//total_energy += h_step*(1.0 / double(j*h_step))*pow(Wave_function(j), 2)*density;
 		total_energy += pow(Wave_function(j), 2)*density / double(j);
 	}
 
+	cout << "interakcna energia: " << 2 * total_energy / pow(density_norm, 2) << endl;
+	cout << "energia HF orbitalu: " << energy << endl;
+
 	total_energy = 2 * energy - 2 * total_energy / pow(density_norm, 2);
 
 	return total_energy;
 }
 
+
+//nieco nieje uplne ok vlnova funkcia nesplna lavu okrajovu podmienku
+//pravdepodobne to je ok lebo prisne vzate integrujem az od h_step
 int main()
 {
 	const double pi = 3.1415926535897932384626433;
 	const double cut_off = 14; //14 je rozumny cutoff
-	const int matrix_size = 1001;
-	const double h_step = cut_off / double(301);
+	const int matrix_size = 4000;
+	const double h_step = cut_off / double(matrix_size);
 	const double delta_epsilon = 10E-8;
 
-	MatrixXd Hamiltonian; //pouzivaju sa hodnoty typu double kôli rýchlosti
+	MatrixXd Hamiltonian; //pouzivaju sa hodnoty typu double kÃ´li rÃ½chlosti
 	VectorXd Wave_function;
 	SelfAdjointEigenSolver<MatrixXd> es; //selfadjoint je pre hermitovske matice
 	
 	double epsilon_new, epsilon_old;
 	double norm;
 	double total_energy;
-	int i;
+	int i, j;
 
 #pragma region inicializacia
 
@@ -125,8 +133,6 @@ int main()
 
 	cout << "computing and diagonalizing hamiltonian in each step will take aproximatelly: " << 2 * 2.2222*10E-8*pow(matrix_size, 3) << "s" << endl << endl;
 	
-
-
 	//inicializacia casovych premennych
 	auto time_start = chrono::high_resolution_clock::now();
 	auto time_end = chrono::high_resolution_clock::now();
@@ -147,10 +153,9 @@ int main()
 
 	//initial wavefunction guess
 	for (i = 0; i < matrix_size; i++)
-		//Wave_function(i) = h_step*i*exp((-1)*pow((i*h_step -0.98) / (double(matrix_size) / 1000.0), 2));
 		Wave_function(i) = sin(pi*i / (matrix_size - 1));
 
-	//cout << "initial wavefunction is: " << endl << Wave_function << endl << endl;
+	cout << "boundary conditions: " << Wave_function(0) << "\t" << Wave_function(matrix_size - 1) << endl;
 
 	//cyklus
 	i = 0;
@@ -167,7 +172,6 @@ int main()
 
 		//naplnit Hamiltonin hodnotami
 		Hamiltonian = Hamiltonian_compute(Wave_function, h_step, matrix_size, norm);
-		//cout << "hamiltonian: " << endl << Hamiltonian << endl;
 		/*for (j = 0; j < matrix_size; j++)
 		{
 			if (j - 1 >= 0 && j + 1 <= matrix_size - 1)
@@ -182,22 +186,37 @@ int main()
 		cout << "diagonalizing hamiltonian... " << endl;
 
 		//diagonalizacia hamiltonianu
-		es.compute(Hamiltonian);
+		//es.compute(Hamiltonian);
 
+		// Construct matrix operation object using the wrapper class DenseGenMatProd
+		DenseSymMatProd<double> op(Hamiltonian);
 
-		time_end = chrono::high_resolution_clock::now();
-		cout << "hamiltonian diagonalized sucesfully in: " << chrono::duration_cast<chrono::nanoseconds>(time_end - time_start).count()*10E-10 << "s" << endl << endl;
+		// Construct eigen solver object, requesting the largest three eigenvalues
+		SymEigsSolver< double, SMALLEST_ALGE, DenseSymMatProd<double> > eigs(&op, 1, 50);
 
-		//vlastne hodnoty by mali byt usporiadane od najmensej po najväèšiu
-		epsilon_new = es.eigenvalues()[0];
-		cout << "Lowest eigenvalue is:" << es.eigenvalues()[0] << endl;
-		cout << "difference of eigenvalues: " << abs(epsilon_new - epsilon_old) << endl << endl;
+		// Initialize and compute
+		eigs.init();
+		int nconv = eigs.compute();
 
-		//cout << "eigenvalues are: " << endl << es.eigenvalues() << endl << endl;
-		//cout << "Eigenvector with lowest eigenvalule is:" << endl << es.eigenvectors().col(iterator) << endl << endl;
+		if (eigs.info() == SUCCESSFUL)
+		{
+			time_end = chrono::high_resolution_clock::now();
+			cout << "hamiltonian diagonalized sucesfully in: " << chrono::duration_cast<chrono::nanoseconds>(time_end - time_start).count()*10E-10 << "s" << endl << endl;
 
-		//priradenie novej vlnovej funkcie
-		Wave_function = es.eigenvectors().col(0);
+			//vlastne hodnoty by mali byt usporiadane od najmensej po najvÃ¤ÄÅ¡iu
+			epsilon_new = eigs.eigenvalues()[0];
+			cout << "Lowest eigenvalue is:" << eigs.eigenvalues()[0] << endl;
+			cout << "difference of eigenvalues: " << abs(epsilon_new - epsilon_old) << endl << endl;
+
+			//cout << "eigenvalues are: " << endl << es.eigenvalues() << endl << endl;
+			//cout << "Eigenvector with lowest eigenvalule is:" << endl << es.eigenvectors().col(iterator) << endl << endl;
+
+			//priradenie novej vlnovej funkcie
+			Wave_function = eigs.eigenvectors().col(0);
+
+			cout << "boundary conditions: " << Wave_function(0) << "\t" << Wave_function(matrix_size - 1) << endl;
+		}
+		else cout << "diagonalization unsuccessfull" << endl;
 
 		time_total_end = chrono::high_resolution_clock::now();
 		cout << "actuall iteration time: " << chrono::duration_cast<chrono::nanoseconds>(time_total_end - time_cycle_start).count()*10E-10 << "s" << endl;
@@ -207,14 +226,14 @@ int main()
 	}
 
 	//celkova energia
-	total_energy = Energy(es.eigenvectors().col(0), es.eigenvalues()[0], matrix_size, h_step, norm);
+	total_energy = Energy(Wave_function, epsilon_new, matrix_size, h_step, norm);
 	cout << "total energy is: " << total_energy << endl << endl;
 
 	ofstream outfile1;
 	outfile1.open("function.dat");
-	for (i = 1; i < matrix_size; i++)
+	for (i = 0; i < matrix_size; i++)
 	{
-		outfile1 << i*h_step << "\t" << Wave_function(i) << endl;
+		outfile1 << (i+1)*h_step << "\t" << Wave_function(i) << endl;
 	}
 	outfile1.close();
 
